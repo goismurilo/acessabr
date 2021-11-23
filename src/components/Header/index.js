6e7efc6afb7fcee8f9556237e1ee62cc0f9805e4
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-11-22 22:56:25 -0300

    feat(Header): Create componet Header page

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './styles.scss';
+
+import logoAcessaBR from '../../images/logo-acessaBR.svg'
+import logoEbac from '../../images/logo-ebac.svg'
+
+const Header = () => {
+    return (
+        <header className="header__container">
+            <div className="header__logo header__logo--state">
+                <img src={logoAcessaBR} alt="Logo AcessaBR" />
+                <span className="header__city">/ Eunápolis - BA</span>
+            </div>
+            <div className="header__logo--ebac">
+                <span>Apoio:</span>
+                <img src={logoEbac} alt="Logo EBAC" />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
