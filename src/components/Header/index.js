5005722f729377c5a5b2d9beea9343855f52564f
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-11-23 21:53:22 -0300

    feat(Header): Add  First Session

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ import './styles.scss';
 import logoAcessaBR from '../../images/logo-acessaBR.svg'
 import logoEbac from '../../images/logo-ebac.svg'
 
-const Header = () => {
+const Header = props => {
     return (
         <header className="header__container">
             <div className="header__logo header__logo--state">
                 <img src={logoAcessaBR} alt="Logo AcessaBR" />
-                <span className="header__city">/ Eunápolis - BA</span>
+                <span className="header__logo__city">/ Eunápolis - BA</span>
             </div>
             <div className="header__logo--ebac">
                 <span>Apoio:</span>
