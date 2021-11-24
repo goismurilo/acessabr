2e8c5d128a8f6717e8cfa1705912b859ce521d7e
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-11-23 22:05:00 -0300

    refactor(Project): Add props City and State

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Header from './components/Header';
+import Home from './pages/Home';
+
 import './styles/global.scss'
+
+const CONFIG = {
+    city: 'Eunápolis',
+    state: 'BA',
+}
 
 ReactDOM.render(
     <div className="main__wrapper">
         <div className="main__container">
-            <Header />
+            <Header city={CONFIG.city} state={CONFIG.state} />
+            <Home city={CONFIG.city} />
         </div>
     </div>,
     document.getElementById('root')
